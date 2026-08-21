430bf84cab0c52ace60680c2831314b1558fe438
Harshitha Reddy <harshithamoola99@gmail.com>
2026-08-21 14:27:17 +0000

    Fix frontend AWS deployment credeFix frontend EKS deployment regionntials

--- setup/terraform/variables.tf
+++ setup/terraform/variables.tf
@@ -1,19 +1,23 @@
 variable "k8s_version" {
-  default = "1.29"
+  type        = string
+  description = "Kubernetes version for the EKS cluster"
+  default     = "1.33"
 }
 
 variable "enable_private" {
-  default = false
+  type        = bool
+  description = "Enable private EKS endpoint and private resources"
+  default     = false
 }
 
 variable "public_az" {
   type        = string
-  description = "Change this to a letter a-f only if you encounter an error during setup"
+  description = "Availability Zone suffix for the public subnet"
   default     = "a"
 }
 
 variable "private_az" {
   type        = string
-  description = "Change this to a letter a-f only if you encounter an error during setup"
+  description = "Availability Zone suffix for the private subnet"
   default     = "b"
 }
